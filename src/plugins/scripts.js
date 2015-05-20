3b30995c6d0ac2e3a3cf749e16f4e26c0f48b9dd
Samuel Colbran <contact@samuco.net>
2015-05-20 16:10:52 +1000

    added time analytics

--- src/plugins/scripts.js
+++ src/plugins/scripts.js
@@ -79,7 +79,11 @@ function getScriptInfo(script, criteria, callback) {
                     if (typeof save_json.criteria === 'undefined') {
                         version = 1;
                     } else {
-                        version = 2;
+                        if (typeof save_json.analytics === 'undefined') {
+                            version = 2;
+                        } else {
+                            version = 3;
+                        }
                     }
                 }
 
@@ -89,14 +93,44 @@ function getScriptInfo(script, criteria, callback) {
                     mark: "",
                     comments: ""
                 };
+                var activity = [];
+                var opened = 0;
                 if (version == 1) {
                     criteria_save = save_json;
-                } else if (version == 2) {
+                } else if (version == 2 || version == 3) {
                     criteria_save = save_json.criteria;
                     meeting_save = save_json.meeting;
+                    if (version == 3) {
+                        activity = save_json.analytics.activity;
+                        opened = save_json.analytics.opened;
+                    }
                 }
 
+                // Calculate the amount of marking time [not interview] (in seconds) spend on this assignment
+                var total_seconds = 0;
+                for (var a = 0; a < activity.length; a++) {
+                    var act = activity[a];
+                    if (act.interview == "false") {
+                        var start = act.start;
+                        var end = act.end;
+                        var seconds = (new Date(end) - new Date(start)) / 1000.0;
+                        total_seconds += seconds;
+                    }
+                }
 
+                var time = "";
+                if (total_seconds > 60) {
+                    var mins = Math.round(total_seconds / 60);
+                    if (mins == 1) {
+                        time = mins + " min";
+                    } else {
+                        time = mins + " mins"
+                    }
+                } else {
+                    time = "-";
+                }
+
+                // Calculate the assignment marks
                 var mark = -1.0;
                 if (typeof save_json !== 'undefined') {
                     mark = criteria_util.calculateMarks(criteria, criteria_save).toPrecision(2);
@@ -111,7 +145,8 @@ function getScriptInfo(script, criteria, callback) {
                     modified: exists,
                     mark: mark,
                     testsRun: total_tests,
-                    testsPassed: total_passed
+                    testsPassed: total_passed,
+                    time: time
                 })
             }
 
--- src/resources/HTML/public/js/edit.js
+++ src/resources/HTML/public/js/edit.js
@@ -230,6 +230,7 @@ function jumpTo(regex) {
     }
 }
 
+// TODO - duplicate code
 function updateGeneral(final, meeting_mark) {
     var codeMark = updateMarks(criteria);
 
@@ -266,6 +267,7 @@ function updateGeneral(final, meeting_mark) {
     return codeMark;
 }
 
+//TODO - CSSE1001 specific code
 function updateMeeting() {
     var icomm = document.getElementById("interview-comments");
     var strings = chunkString(icomm.value, 78);
@@ -289,6 +291,7 @@ function updateMeeting() {
     editor.scrollToRow(row + (newLines-oldLines));
 }
 
+//TODO - CSSE1001 specific code
 function updateTotal(final, meeting_mark, code_mark) {
     var total_mark = Math.min(meeting_mark, code_mark);
     var output = "";
@@ -316,6 +319,7 @@ function updateTotal(final, meeting_mark, code_mark) {
 
 }
 
+//TODO - CSSE1001 specific code
 function updateComments(final) {
     var imark = document.getElementById("interview-mark").value;
     var meeting_mark = parseInt(imark);
@@ -326,6 +330,46 @@ function updateComments(final) {
     var code_mark = updateGeneral(final, meeting_mark);
     updateMeeting();
     updateTotal(final, meeting_mark, code_mark);
+}
+
+
+// Analytics
+// Activity - http://www.kirupa.com/html5/detecting_if_the_user_is_idle_or_inactive.htm
+var timeoutID;
+var prev_inactivity = new Date();
+function setupActivity() {
+    this.addEventListener("mousemove", resetTimer, false);
+    this.addEventListener("mousedown", resetTimer, false);
+    this.addEventListener("keypress", resetTimer, false);
+    this.addEventListener("DOMMouseScroll", resetTimer, false);
+    this.addEventListener("mousewheel", resetTimer, false);
+    this.addEventListener("touchmove", resetTimer, false);
+    this.addEventListener("MSPointerMove", resetTimer, false);
+
+    startTimer();
+}
+function startTimer() {
+    // wait 10 seconds before calling goInactive
+    timeoutID = window.setTimeout(goInactive, 10000);
+}
+function resetTimer(e) {
+    window.clearTimeout(timeoutID);
+
+    goActive();
+}
+
+function goInactive() {
+    var now = new Date();
+    activity.push({
+        start: prev_inactivity,
+        end: now,
+        interview: interview
+    });
+    prev_inactivity = now;
+}
+
+function goActive() {
+    startTimer();
 }
 
 
@@ -349,6 +393,9 @@ function save() {
     // Save the comment and checkbox values
     var exported = {};
     exportChildren(criteria, exported);
+
+    // Flush analytics
+    goInactive();
 
     $.ajax({
         xhr: function()
@@ -374,6 +421,10 @@ function save() {
                     mark: imark,
                     comments: icomm
                 },
+                analytics: {
+                    activity: activity,
+                    opened: opened
+                },
                 criteria: exported
             }
         },
@@ -390,10 +441,13 @@ function save() {
 
 $(document).ready(function() {
 
+    // Analytics
+    setupActivity();
+    opened++;
+
     // Interview
     var imark = document.getElementById("interview-mark");
     var icomm = document.getElementById("interview-comments");
-
     function updateInterviewed(start) {
         var interview = $($('#interviewed')).prop('checked');
         if (!interview) {
